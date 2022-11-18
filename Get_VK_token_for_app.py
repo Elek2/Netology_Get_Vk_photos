31104aa3425048172f66ec3a7b6caa8b514c7c87
Elek <Elek2@yandex.ru>
2022-11-18 05:02:21 +0300

    Добавлен ввод пользователем токена и id, доделка под PEP8, добавлен Requirements, добавлена программа получения токена VK

--- Get_VK_token_for_app.py
+++ Get_VK_token_for_app.py
@@ -1,18 +1,23 @@
 import webbrowser
 
-base_url = 'https://oauth.vk.com/authorize?'
-params = {
-	'client_id': 51475199,
-	'display': 'page',
-	'scope': 'friends,photos,stories,offline,messages',
-	'redirect_uri': 'https://oauth.vk.com/blank.html',
-	'response_type': 'token',
-	'v': '5.131',
-	'state': 'Well done'
-}
-url_params = "&".join([f'{k}={v}' for k, v in params.items()])
-url = base_url + url_params
-print(url)
+
+def open_url(app_id):
+	base_url = 'https://oauth.vk.com/authorize?'
+	params = {
+		'client_id': app_id,
+		'display': 'page',
+		'scope': 'friends,photos,stories,offline,messages',
+		'redirect_uri': 'https://oauth.vk.com/blank.html',
+		'response_type': 'token',
+		'v': '5.131',
+		'state': 'Well done'
+	}
+	url_params = "&".join([f'{k}={v}' for k, v in params.items()])
+	url = base_url + url_params
+	print(url)
+	webbrowser.open(url, new=2)
 
 
-webbrowser.open(url, new = 2)
+if __name__ == '__main__':
+	your_app_id = 3456
+	open_url(your_app_id)
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import logging
 
 class GetPhotos:
 	def __init__(self, vk_id, ya_token):
+		# Настройка конфигурации VK
 		self.vk_id = vk_id
 		self.vk_token = open('VK_token.txt', 'r').read()
 		self.base_VK_url = 'https://api.vk.com/method/'
@@ -14,19 +15,21 @@ class GetPhotos:
 			'access_token': self.vk_token,
 			'v': '5.131',
 		}
+		# Настройка конфигурации Я.Диск
 		self.ya_token = ya_token
 		self.base_Ya_url = 'https://cloud-api.yandex.net/v1/disk/'
 		self.Ya_headers = {
 			'Content-Type': 'application/json',
 			'Authorization': f'OAuth {self.ya_token}'
 		}
-
+		# Настройка логирования
 		logging.basicConfig(
 			filename="logs.log",
 			level=logging.INFO,
 			format="%(asctime)s %(levelname)s %(message)s"
 		)
 
+	# Получаем имя пользователя VK по его ID и проверяем на ошибки (удаленный, приватный пользователь)
 	def _get_name(self):
 		end_url = 'users.get'
 		vk_params = {
@@ -45,7 +48,7 @@ class GetPhotos:
 		except IndexError:
 			logging.error("Имя пользователя не получено")
 
-	@property
+	# Получаем фотографии пользователя VK по его ID
 	def _get_vk_photos(self):
 		end_url = 'photos.get'
 		vk_params = {
@@ -63,16 +66,22 @@ class GetPhotos:
 		except TypeError:
 			logging.error("Фотографии не получены")
 
+	# Приводим полученную информацию по фотографиям в божеский вид
 	def _reformat_photos_list(self, photos_list):
-		colums = ['name', 'size_type', 'photo_url']
-		likes_list = [photo['likes']['count'] for photo in photos_list]
+		colums = ['name', 'size_type', 'photo_url']  # Оставляем только необходимые ключи словаря
+		likes_list = [photo['likes']['count'] for photo in photos_list]  # Считаем лайки каждой фотографии
 		reformat_photos_list = []
+
+		# каждая фотография храниться в нескольких размерах. Каждому соответствует буква алфавита в поле ['type']
+		# находим фото с последней по алфавиту буквой, с лучшим размером фото, остальные удаляем
 		for photo in photos_list:
 			max_photo_size = max(photo['sizes'], key=lambda x: x['type'])
 			max_photo = list(filter(lambda x: x['type'] == max_photo_size['type'], photo['sizes']))
 			photo['photo_url'] = max_photo[0]['url']
 			photo['size_type'] = max_photo[0]['type']
 
+			# В качестве имени фото используем количество лайков
+			# Если у нескольких фото количество одиннаково, добавляем к имени дату публикации фото
 			if likes_list.count(photo["likes"]['count']) > 1:
 				date = str(datetime.utcfromtimestamp(photo["date"]).strftime('%Y-%m-%d'))
 				name = f'{str(photo["likes"]["count"])}_{date}.jpg'
@@ -80,10 +89,11 @@ class GetPhotos:
 				name = f'{str(photo["likes"]["count"])}.jpg'
 			photo['name'] = name
 			photo['likes'] = photo['likes']['count']
-			photo = {key: value for key, value in photo.items() if key in colums}
+			photo = {key: value for key, value in photo.items() if key in colums}  # Получаем готовый вид
 			reformat_photos_list.append(photo)
 		return reformat_photos_list
 
+	# Создаем папку на яндекс.диске
 	def _create_folder_on_disk(self, folder_name="Фотографии с VK"):
 		end_url = 'resources'
 		url = self.base_Ya_url + end_url
@@ -95,6 +105,7 @@ class GetPhotos:
 		except:
 			logging.error("Папка на Яндекс.Диске НЕ создана")
 
+	# Загружаем фото на яндекс.диск
 	def _upload_to_disk(self, file_name, photo_url):
 		end_url = 'resources/upload'
 		url = self.base_Ya_url + end_url
@@ -102,34 +113,34 @@ class GetPhotos:
 		response = requests.post(url, headers=self.Ya_headers, params=params)
 		return response
 
+	# Основная программа, собирающая остальные
 	def get_photos(self):
-		full_name = self._get_name()
-		all_photos = self._get_vk_photos
-		needed_photos = self._reformat_photos_list(all_photos)
-		base_folder_name = self._create_folder_on_disk()
-		folder_name = self._create_folder_on_disk(f'{base_folder_name}/{full_name}')
+		full_name = self._get_name()  # Получили имя пользователя
+		all_photos = self._get_vk_photos()  # Получили фото
+		needed_photos = self._reformat_photos_list(all_photos)  # Оставили только нужную инфу
+		base_folder_name = self._create_folder_on_disk()  # Создали общую папку для фото
+		folder_name = self._create_folder_on_disk(f'{base_folder_name}/{full_name}')  # Создали папку с именем пользователя
 
+		# Загрузили все фото в папку
 		for photo in tqdm(needed_photos, desc='Загружено фотографий'):
 			file_name = f'{folder_name}/{photo["name"]}'
 			photo_url = photo["photo_url"]
 			self._upload_to_disk(file_name, photo_url)
 		logging.info(f"Фотографии {len(needed_photos)} шт. загружены на Яндекс.Диск")
 
+		# Загрузили информацию о фото в json-файл
 		photo_json = [{"file_name": photo['name'], "size": photo['size_type']} for photo in needed_photos]
 		with open(f'{full_name}_photo.json', 'w', encoding='utf-8') as f:
 			json.dump(photo_json, f)
-
 		return photo_json
 
 
 if __name__ == '__main__':
-	VK_token = open('VK_token.txt', 'r').read()
-	Ya_token = open('Ya_token.txt', 'r').read()
 
-	photo_bot = GetPhotos(36140, Ya_token)
+	# Ya_token = open('Ya_token.txt', 'r').read()
+	Ya_token = input("Введите токен Яндекс диска: ")
+	user_id = input("Введите id пользователя VK: ")
+
+	photo_bot = GetPhotos(user_id, Ya_token)
 	photo_bot.get_photos()
 
-# 5327 - private
-# 1243256126 - deleted
-# 1234569789578354 - нет
-# 36140 = я
